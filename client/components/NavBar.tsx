d0ecdd498b759984b230b1d834eaa6e8a34657ee
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-08 20:27:50 -0800

    Updated nav button

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -6,9 +6,16 @@ import { Box, Button, ButtonGroup, Flex, Text } from "@chakra-ui/react";
 function NavBar() {
   return (
     <Box>
-      <Flex my={15} mx={200} justify="space-between">
+      <Flex my={15} mx={150} justify="space-between">
         <Link href="/">
-          <Flex align="center" justify="center" p={1} as="button" _hover={{ bg: "gray.100" }}>
+          <Flex
+            align="center"
+            justify="center"
+            p={1}
+            as="button"
+            borderRadius="md"
+            _hover={{ bg: "gray.100" }}
+          >
             <Image src="/images/winged-shoe-icon.png" alt="hermes logo" width={70} height={70} />
             <Flex alignItems="center" justifyContent="center">
               <Text fontSize="4xl">Hermes</Text>
