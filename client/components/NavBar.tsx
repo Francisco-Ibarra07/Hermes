8301405cef02f0b081a6a6375bf5f7896a6320e5
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-08 19:51:39 -0800

    Created NavBar component

---
+++ client/components/NavBar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Box, Button, ButtonGroup, Flex, Text } from "@chakra-ui/react";
+
+function NavBar() {
+  return (
+    <Box>
+      <Flex my={15} mx={200} justify="space-between">
+        <Link href="/">
+          <Flex align="center" justify="center" p={1} as="button" _hover={{ bg: "gray.100" }}>
+            <Image src="/images/winged-shoe-icon.png" alt="hermes logo" width={70} height={70} />
+            <Flex alignItems="center" justifyContent="center">
+              <Text fontSize="4xl">Hermes</Text>
+            </Flex>
+          </Flex>
+        </Link>
+
+        <Flex align="center" justify="center">
+          <ButtonGroup ml={2} variant="outline">
+            <Link href="/login">
+              <Button>Login</Button>
+            </Link>
+            <Link href="/signup">
+              <Button>Signup</Button>
+            </Link>
+          </ButtonGroup>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
+
+export default NavBar;
