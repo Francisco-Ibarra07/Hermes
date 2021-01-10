ba7ed3b82ab2fa7ebfa4778b7fea237230759fc2
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-09 23:39:36 -0800

    Setup mikro-orm migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
   console.log("started");
 
   const orm = MikroORM.init(mikroConfig);
+  await (await orm).getMigrator().up();
+
+  const post = (await orm).em.create(Post, { title: "test post" });
+  await (await orm).em.persistAndFlush(post);
 };
 
 main();
