714f4b13d03b35d65145840854e725f2f947248f
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-10 16:09:19 -0800

    Finished CRUD operations for PostResolver

--- server/src/resolvers/PostResolver.ts
+++ server/src/resolvers/PostResolver.ts
@@ -1,13 +1,73 @@
 import { Post } from "../entities/Post";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "src/types";
 
 @Resolver()
 export class PostResolver {
-  @Query(() => [Post]) // Returns an array of post
+  // Returns an array of post
+  @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     const { em } = ctx;
 
     return em.find(Post, {});
+  }
+
+  // Returns a single post given an id
+  // Returns 'null' if not found
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id", () => Int) id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
+    const { em } = ctx;
+
+    // Find post where id='{id}'
+    return em.findOne(Post, { id });
+  }
+
+  // Creates a single post given the title
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post> {
+    const { em } = ctx;
+
+    // Create new post with title passed in
+    const newPost = em.create(Post, { title });
+    await em.persistAndFlush(newPost);
+    return newPost;
+  }
+
+  // Updates a single post given id and title
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id", () => Int) id: number,
+    @Arg("title", () => String) newTitle: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
+    const { em } = ctx;
+
+    // Make sure it exists
+    const targetPost = await em.findOne(Post, { id });
+    if (!targetPost) {
+      return null;
+    }
+
+    // Use em to update on postgres
+    targetPost.title = newTitle;
+    await em.persistAndFlush(targetPost);
+
+    return targetPost;
+  }
+
+  // Deletes a single post given an id
+  @Mutation(() => Boolean)
+  async deletePost(@Arg("id", () => Int) id: number, @Ctx() ctx: MyContext): Promise<boolean> {
+    const { em } = ctx;
+
+    try {
+      await em.nativeDelete(Post, { id });
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
