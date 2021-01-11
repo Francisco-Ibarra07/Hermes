5c882f498751653c79612dd7257ce62d8cef4789
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-11 13:45:04 -0800

    Installed urql to perfom graphql calls

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,14 +1,20 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+import { AppProps } from "next/app";
+import { createClient, Provider } from "urql";
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+const client = createClient({
+  url: "http://localhost:5000/graphql",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
