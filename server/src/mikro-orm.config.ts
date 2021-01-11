7db5a436157ad82730ba2a88759ec8136509dc96
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-10 16:20:54 -0800

    Created new migration

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 const ormConfig = {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "hermes",
   user: "fibarra",
   password: "fibarra",
