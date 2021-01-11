9c8211283d2a28ed69d8dfb97749a3270932050d
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-10 16:57:55 -0800

    Added loginUser mutation

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,12 +1,30 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, Mutation, ObjectType, Resolver } from "type-graphql";
 import argon2 from "argon2";
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
 
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
-  async registerUser(
+  async signupUser(
     @Arg("email", () => String) email: string,
     @Arg("password", () => String) password: string,
     @Ctx() ctx: MyContext
@@ -14,9 +32,48 @@ export class UserResolver {
     const { em } = ctx;
     const hashedPassword = await argon2.hash(password);
 
-    const newUser = em.create(User, { email, password: hashedPassword });
+    const newUser = em.create(User, { email: email.toLowerCase(), password: hashedPassword });
     await em.persistAndFlush(newUser);
 
     return newUser;
+  }
+
+  @Mutation(() => UserResponse)
+  async loginUser(
+    @Arg("email", () => String) email: string,
+    @Arg("password", () => String) password: string,
+    @Ctx() ctx: MyContext
+  ): Promise<UserResponse> {
+    const { em } = ctx;
+
+    // If user DNE, return error
+    const user = await em.findOne(User, { email: email.toLowerCase() });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "email",
+            message: "Invalid login",
+          },
+        ],
+      };
+    }
+
+    // If invalid password, return error
+    const isValidPassword = await argon2.verify(user.password, password);
+    if (!isValidPassword) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Invalid login",
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
