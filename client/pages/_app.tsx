b918fe05890d0736f77dfc5fdd6252271364e901
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-16 22:13:12 -0800

    Created a wrapper function that calls urql update query

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,52 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import { AppProps } from "next/app";
-import { createClient, Provider } from "urql";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
+import { IsLoggedInQuery, LoginDocument, LoginMutation } from "../generated/graphql";
+
+function updateQueryWrapper<Result, Query>(
+  result: any,
+  cache: Cache,
+  input: QueryInput,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(input, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:5000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          loginUser: (_result, args, cache) => {
+            updateQueryWrapper<LoginMutation, IsLoggedInQuery>(
+              _result,
+              cache,
+              { query: LoginDocument },
+              (result, query) => {
+                console.log("result: ", result);
+                console.log("query: ", query);
+                if (result.loginUser.errors) {
+                  return query;
+                } else {
+                  return {
+                    isLoggedIn: result.loginUser.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
