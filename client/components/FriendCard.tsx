5b2099066c487c401c335312a5136fb319d4339e
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-17 01:20:38 -0800

    Added name prop to FriendCard component

--- client/components/FriendCard.tsx
+++ client/components/FriendCard.tsx
@@ -2,6 +2,7 @@ import { Flex, Button, Text, Image as ChakraImage } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 interface FriendCardProps {
+  name: string;
   cardKey: number;
   isActive?: boolean;
   onClickHandler: (key: number) => void;
@@ -28,7 +29,9 @@ function FriendCard(props: FriendCardProps) {
       <Flex align="center">
         <ChakraImage src="/images/avatar-icon.png" alt="avatar" boxSize={30} />
         <Flex ml={2} maxW="80%" flexDir="column" justify="center">
-          <Text fontSize="sm">John Smith</Text>
+          <Text fontSize="sm" isTruncated>
+            {props.name}
+          </Text>
           <Text fontSize="xs" isTruncated>
             You: Last message text a very long message indeed
           </Text>
